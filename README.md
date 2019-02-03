6c4738c45c9563e092e0eee2627dbf2e3d6d7248
jforan <foran.jlb@gmail.com>
2019-02-03 17:44:07 -0500

    proj file runs & readme updated

---
+++ README.md
@@ -0,0 +1,35 @@
+# Functionality
+
+This app allows for a number of possibile outcomes. 
+
+* it welcomes users to the app
+* welcomes it to a specific bank
+* determines a value in a specific currency & displays the value in a more easily read-able format
+* it then brings it all together to combine all those components into one output
+
+
+
+
+
+# Requirements
+* Flask
+* lab3_code.py (this provides Currency, Yuan, Dollar, Pound, and Bank classes)
+
+note: all requirements can be found acquired by running the requirements.txt file.
+
+
+
+
+# How to Run
+
+## Type in the command: python SI507_project1.py runserver
+
+## Possible thing to type in the URL
+
+note: you will change things in <>. it will begin with either http://127.0.0.1:5000/ or http://localhost:5000/, followed by the below options. typing in the URLs mentioned will bring users to the homepage.
+
+* /bank/<name of bank>
+* /dollar/<amt (number not written out)>
+* /yuan/<amt (number not written out)>
+* /pound/<amt (number not written out)>
+* /bank/<name>/<currency>/<value (number not written out)>
--- SI507_project1.py
+++ SI507_project1.py
@@ -11,15 +11,55 @@ def basic():
     return 'Welcome to the banking app!'
 
 
-
 @app.route('/bank/<name>')
 def welcome_name(name):
-    bank_name = Bank(name)
-    return "Welcome to {}!".format(bank_name)
+    bank_insts = Bank(name, Dollar, 1)
+    bank_name = bank_insts.name
+    return 'Welcome to {}!'.format(bank_name)
 
-# Something else to note is that the functions don't get INVOKED the way you may be used to. Running the application and *going to a URL that matches the path in the @app.route() business IS what runs that function!
+
+@app.route('/dollar/<amt>')
+def dollar_app(amt):
+    num = int(amt)
+    num_insts = Dollar(num)
+    return num_insts.__str__()
 
 
-@app.route('/showvalues/<name>/<greeting>')
-def showvalues(name, greeting): # Check this out -- what's going on here? A useful pattern to think about.
-    return "{}, {}!".format(greeting, name)
+@app.route('/yuan/<amt>')
+def yuan_app(amt):
+    num = int(amt)
+    num_insts = Yuan(num)
+    return num_insts.__str__()
+
+
+@app.route('/pound/<amt>')
+def pound_app(amt):
+    num = int(amt)
+    num_insts = Pound(num)
+    return num_insts.__str__()
+
+
+@app.route('/bank/<name>/<currency>/<value>')
+def final(name,currency,value):
+    new_currency = currency.lower()
+    if new_currency == 'dollar':
+        num = int(value)
+        num_insts = Dollar(num)
+        return 'Welcome to the {} bank! {} Bank holds the {} currency and currently holds {}.'.format(name,name, new_currency, num_insts.__str__())
+
+    if new_currency == 'yuan':
+        num = int(value)
+        num_insts = Yuan(num)
+        return 'Welcome to the {} bank! {} Bank holds the {} currency and currently holds {}.'.format(name,name, new_currency, num_insts.__str__())
+
+    if new_currency == 'pound':
+        num = int(value)
+        num_insts = Pound(num)
+        return 'Welcome to the {} bank! {} Bank holds the {} currency and currently holds {}.'.format(name,name, new_currency, num_insts.__str__())
+
+    else:
+        return 'Invalid URL inputs for bank.'
+
+
+if __name__ == "__main__":
+    app.run()
